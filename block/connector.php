6d8fafe08ffbdc4dcd61b3249a4c187bae0ee5ca
Hauke Mewes <haukemewes@googlemail.com>
2012-09-27 16:50:36 +0200

    added date function for database

--- block/connector.php
+++ block/connector.php
@@ -17,6 +17,18 @@ function exQuery(mysqli_stmt $query, $affected = 0) {
     return $query->execute() && $query->affected_rows > $affected;
 }
 
+/*
+ * Creates a timestamp in the format of the database
+ * @param date if not specified the current time
+ */
+function dateForDatabase($date = null)
+{
+    if ($date === null) {
+        $date = time();
+    }
+    return date("Y-m-d H:i:s", $date);
+}
+
 // Defines tables
 $prefix = /*PREFIX*/"bl_"/*/PREFIX*/;
 define("CONTENT", $prefix . "content");
