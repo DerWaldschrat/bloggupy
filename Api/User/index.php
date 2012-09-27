e4ec9adf5973539a2d61341014fcc4dbbd0b3f31
Hauke Mewes <haukemewes@googlemail.com>
2012-09-27 19:41:20 +0200

    added Api/ dir which contains all the backend logic

---
+++ Api/User/index.php
@@ -0,0 +1,23 @@
+<?php
+define("__EXEC", true);
+define("ROOT", "../../");
+define("PHP", ".php");
+require ROOT . "block/user" . PHP;
+require ROOT . "block/mapper" . PHP;
+require ROOT . "block/validator" . PHP;
+post(function () {
+    $user = bodyAsJSON();
+    if (hasAllSet($user, array("name", "password")) && $user->name === /*USERNAME*/"DerWaldschrat"/*/USERNAME*/ && $user->password === /*USERPASS*/"qwerty"/*/USERPASS*/) {
+        $user = $_SESSION["user"] = array("name" => $user->name, "loggedin" => true, "rights" => 1000, "id" => 1);
+        hJSON($user);
+    } else {
+        h404();
+    }
+});
+// Log out
+delete(function () {
+    if (isset($_SESSION["user"])) {
+        unset($_SESSION["user"]);
+    }
+})
+?>
