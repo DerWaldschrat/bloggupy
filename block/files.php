5f3f4690dab5b8a34243e6dc95edf8049403b23d
Hauke Mewes <haukemewes@googlemail.com>
2012-09-27 17:14:03 +0200

    added file system functions

---
+++ block/files.php
@@ -0,0 +1,20 @@
+<?php
+defined("__EXEC") or exit;
+// Contains functionality for file system
+
+// Creates gzipped file
+function gz_file_put_contents($file, &$data)
+{
+    $fp = gzopen($file, "w9");
+    gzwrite($fp, $data);
+    gzclose($fp);
+}
+
+// Creates gzipped file from other file
+function gz_file_from_file($source, $new)
+{
+    $data = implode("", file($source));
+    gz_file_put_contents($new, $data);
+    unset($data);
+}
+?>
