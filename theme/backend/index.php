0e912bedaf1192b43a3f0bb8426e0192994d70a6
Hauke Mewes <haukemewes@googlemail.com>
2012-09-27 19:02:25 +0200

    created backend theme with navbar and login screen

--- theme/backend/index.php
+++ theme/backend/index.php
@@ -5,19 +5,44 @@
         <style type="text/css">
             @import url(<?php echo CLIENTROOT; ?>assets/vendor/bootstrap/bootstrap.css);
             @import url(<?php echo CLIENTROOT; ?>assets/vendor/bootstrap/responsive.css);
+            @import url(<?php echo CLIENTROOT; ?>theme/main.css)
         </style>
     </head>
     <body>
-    <script>
-        var steal = {env: "production"}
-    </script>
-    <script src="<?php echo CLIENTROOT; ?>assets/vendor/steal/steal.js">
+        <div id="navbarTop" class='navbar navbar-fixed-top'>
+            <div class="navbar-inner">
 
-    </script>
-    <script>
-        steal.rootUrl("<?php echo CLIENTROOT; ?>")
-        // Init page
-        steal("assets/app")
-    </script>
+            </div>
+        </div>
+        <div id="main" class="row-fluid">
+            <div id="navbarLeft" class="span3">
+
+            </div>
+            <div id="content" class="span9">
+                <form id="inputForm" action="#">
+                    <fieldset class="control-group">
+                        <legend>Einloggen</legend>
+                        <label for="name">Name:</label><input type="text" name="name" id="name" autofocus="on" />
+                        <label for="password">Passwort:</label><input type="password" name="password" id="password" />
+                    </fieldset>
+                    <fieldset class="control-group buttonAndMessage">
+                        <button type="submit" class="btn" disabled="disabled">Einloggen</button>
+                        <div class="statusField"></div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+        <script>
+            // Config stealJS
+            var steal = {env: "production"}
+        </script>
+        <script src="<?php echo CLIENTROOT; ?>assets/vendor/steal/steal.js">
+        </script>
+        <script>
+            // Set root
+            steal.rootUrl("<?php echo CLIENTROOT; ?>")
+            // Init page
+            steal("assets/app")
+        </script>
     </body>
 </html>
