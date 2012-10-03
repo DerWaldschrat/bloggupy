a294cf1a504a7de03678de89cc442380b9291fac
Hauke Mewes <haukemewes@googlemail.com>
2012-10-03 18:39:40 +0200

    added updated index to content table

--- sql/content.sql
+++ sql/content.sql
@@ -8,4 +8,6 @@ CREATE TABLE bl_content (
   permalink VARCHAR(100), -- permalink to this entry
   content TEXT, -- content of this entry
   published TINYINT -- whether this article is published or not: 0 is not published, 1 is published for loggedin users, 2 is published for public
-)
+);
+-- Add index to content table
+ALTER TABLE `bl_content` ADD INDEX `updated` ( `updated` );
